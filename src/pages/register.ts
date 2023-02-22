0566eda9c1cb284b030b3a5b1228975e497529dd
sendistephen <sendi.steve2@gmail.com>
2023-02-23 00:00:15 +0300

    wip: Add api functionality to register user

---
+++ src/pages/register.ts
@@ -0,0 +1,32 @@
+import { createJWT, hashPassword } from '@src/app/(dashboard)/project/[id]/auth';
+import { db } from '@src/lib/db';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { serialize } from 'cookie';
+
+export default async function register(req: NextApiRequest, res: NextApiResponse) {
+	// Check if the request was a POST request
+	if (req.method === 'POST') {
+		// Create the new user using data from the request body
+		const user = await db.user.create({
+			data: {
+				email: req.body.email,
+				password: await hashPassword(req.body.password),
+				firstName: req.body.firstName,
+				lastName: req.body.lastName,
+			},
+		});
+		// Create a JWT for the new user
+		const jwt = await createJWT(user);
+		// Set the cookie in the response with the JWT and correct settings
+		res.setHeader(
+			'Set-Cookie',
+			serialize(process.env.COOKIE_NAME, jwt, {
+				httpOnly: true,
+				path: '/',
+				maxAge: 60 * 60 * 24 * 365,
+			})
+		);
+		res.status(201); // Created
+		res.end();
+	}
+}
